21db68e6ecc8ced22910654c18d8a754b43c5712
zajoe10 <zajoe10@gmail.com>
2023-06-15 17:32:03 -0700

    8

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(void){
-    push("\"Programming is like building a multilingual puzzle");
-    return (0);
+
+int main(void)
+{
+	push("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
