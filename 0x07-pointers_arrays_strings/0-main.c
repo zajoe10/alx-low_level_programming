441d9c42b3ed732d950697d15dadc2d9b81e2a59
zajoe10 <zajoe10@gmail.com>
2023-07-04 17:55:08 -0700

    3

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -21,12 +21,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
 		}
 		if (!(i % 10) && i)
 		{
-			printf("\n");
+		printf("\n");
 		}
 		printf("0x%02x", buffer[i]);
 		i++;
 	}
-		printf("\n");
+	printf("\n");
 }
 
 /**
@@ -37,9 +37,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
 int main(void)
 {
 	char buffer[98] = {0x00};
+
 	simple_print_buffer(buffer, 98);
 	_memset(buffer, 0x01, 95);
 	printf("-------------------------------------------------\n");
 	simple_print_buffer(buffer, 98);
 	return (0);
 }
+
